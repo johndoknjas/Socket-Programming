122d6581e1ca04ecd6ddf8e7bf466750a54a0243
John Doknjas <jdoknjas@sfu.ca>
2020-06-25 17:18:22 -0700

    testing communication

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Pipe, Queue
+import time
+from random import randint
+import sys
 
-def sender_func(conn):
-    pass
+def sender_func(conn, q):
+    # For now, generate random data for packets.
+    print(randint(0,1))
+    
+    while True:
+        q.put([randint(0,1024), True if randint(0,1) == 0 else False,
+              True if randint(0,1) == 0 else False, 
+              True if randint(0,1) == 0 else False])
+        
+        time.sleep(2)
+    
 
-def receiver_func(conn):
-    pass
+def receiver_func(conn, q):
+    
+    while True:
+        print(q.get())
 
 
 def main():
     sender_conn, receiver_conn = Pipe()
     
-    p1 = Process(target=sender_func, args=(sender_conn,))
-    p2 = Process(target=receiver_func, args=(receiver_conn,))
+    q = Queue()
     
+    p1 = Process(target=sender_func, args=(sender_conn, q,))
     p1.start()
+    
+    p2 = Process(target=receiver_func, args=(receiver_conn, q,))
     p2.start()
-    
-    
     
     p1.join()
     p2.join()
